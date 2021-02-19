f9145647bbf6f00249af4dbbf108b843e8954801
Masaya Shida <masayashida@gmail.com>
2021-02-19 20:14:41 +0700

    datocms connection

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,57 @@
 import { useState, useEffect } from "react";
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import { useCookies } from "react-cookie"
 export default function Home() {
+  const token = "36beb28052fa27ba685e891e385a4b";
   const [lights, setLights] = useState((lights) => {return false}); // by default lights are off
+  useEffect(() => {
+    fetch(
+        'https://graphql.datocms.com/',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            query: `{ 
+                allLights {
+                  boolean
+                }
+              }`
+          }),
+        } 
+    )
+    .then(res => res.json())
+    .then((res) => {
+        console.log(res.data["allLights"][0]["boolean"])
+        setLights(res.data["allLights"][0]["boolean"])
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}, [])
+
+const SiteClient = require("datocms-client").SiteClient;
+const client = new SiteClient(token);
+const itemId = "21519068";
+client.items
+  .update(itemId, {
+    boolean: lights,
+  })
+  .then((item) => {
+    console.log(item);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Team Electron</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
